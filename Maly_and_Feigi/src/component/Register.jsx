eb1cb4f2b297b99059e6c7e3a0f0c227bba73551
Kibelevich <Hand4c28@mbjcomp.org.il>
2024-01-14 12:04:38 +0200

    c

--- Maly_and_Feigi/src/component/Register.jsx
+++ Maly_and_Feigi/src/component/Register.jsx
@@ -45,14 +45,17 @@ function Register({ userDetails, nextId, setNextId }) {//Search Params / Link st
                 if (!response.ok) {
                     throw 'Error' + response.status + ': ' + response.statusText;
                 }
+                else {
+                    alert("user successfully added")
+                    setNextId(prevId => prevId + 1)
+                    //local storage
+                    const arrUsers = JSON.parse(localStorage.getItem('Users')) || [];
+                    arrUsers.push({ name: userDetails.username, password: userDetails.website });
+                    localStorage.setItem('Users', JSON.stringify(arrUsers));
+                    navigate('/home');
+                }
             });
-            alert("user successfully added")
-            setNextId(prevId => prevId + 1)
-            //local storage
-            const arrUsers = JSON.parse(localStorage.getItem('Users')) || [];
-            arrUsers.push({ name: userDetails.username, password: userDetails.website });
-            localStorage.setItem('Users', JSON.stringify(arrUsers));
-            navigate('/home');
+
         } catch (ex) { alert(ex); }
     }
     return (<>
@@ -64,30 +67,30 @@ function Register({ userDetails, nextId, setNextId }) {//Search Params / Link st
             <input name='email' type='email' required></input>
 
             <h4>address:</h4>
-                <label htmlFor='street' >street</label>
-                <input name='street' type='text' required></input>
-                <label htmlFor='suite' >suite</label>
-                <input name='suite' type='text' required></input>
-                <label htmlFor='city' >city</label>
-                <input name='city' type='text' required></input>
-                <label htmlFor='zipcode' >zipcode</label>
-                <input name='zipcode' type='text' required></input>
-                <h4>geo:</h4>
-                    <label htmlFor='lat' >lat</label>
-                    <input name='lat' type='text' required></input>
-                    <label htmlFor='lng' >lng</label>
-                    <input name='lng' type='text' required></input>
+            <label htmlFor='street' >street</label>
+            <input name='street' type='text' required></input>
+            <label htmlFor='suite' >suite</label>
+            <input name='suite' type='text' required></input>
+            <label htmlFor='city' >city</label>
+            <input name='city' type='text' required></input>
+            <label htmlFor='zipcode' >zipcode</label>
+            <input name='zipcode' type='text' required></input>
+            <h4>geo:</h4>
+            <label htmlFor='lat' >lat</label>
+            <input name='lat' type='text' required></input>
+            <label htmlFor='lng' >lng</label>
+            <input name='lng' type='text' required></input>
 
             <label htmlFor='phone' >phone</label>
             <input name='phone' type='text' required></input>
 
             <h4>company:</h4>
-                <label htmlFor='companyName' >name</label>
-                <input name='companyName' type='text' required></input>
-                <label htmlFor='catchPhrase' >catch phrase</label>
-                <input name='catchPhrase' type='text' required></input>
-                <label htmlFor='bs' >bs</label>
-                <input name='bs' type='text' required></input>
+            <label htmlFor='companyName' >name</label>
+            <input name='companyName' type='text' required></input>
+            <label htmlFor='catchPhrase' >catch phrase</label>
+            <input name='catchPhrase' type='text' required></input>
+            <label htmlFor='bs' >bs</label>
+            <input name='bs' type='text' required></input>
 
             <button type='submit'>enter</button>
         </form>
