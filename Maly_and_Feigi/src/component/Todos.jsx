8a3606c7d227ddf386a5e86bc960a62b10ca0df7
feigiz <pz0933@gmail.com>
2024-01-15 19:16:18 +0200

    search

--- Maly_and_Feigi/src/component/Todos.jsx
+++ Maly_and_Feigi/src/component/Todos.jsx
@@ -11,7 +11,8 @@ function Todos() {
     // const [getTodos, setGetTodos] = useState(true);
     const [userTodos, setUserTodos] = useState([]);
     const [showAdditionForm, setShowAdditionForm] = useState(false);
-    const [editable, setEditable] = useState([])
+    const [editables, seteditables] = useState([]);
+    const [todo, setTodo] = useState(null);
 
     function addingTask(event) {
         event.preventDefault();
@@ -24,33 +25,34 @@ function Todos() {
     }
 
     function submitChanges() {
-        try{
-            fetch(`http://localhost:3000/todos?userId=${userDetailes.state.id}`,{
-            method: 'PUT',
-            body: JSON.stringify({
-                userTodos
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        }).then(response => {
-            if (!response.ok) {
-                throw 'Error' + response.status + ': ' + response.statusText;
-            }
-            else {
-                return response.json();
+        try {
+            // fetch(`http://localhost:3000/todos?userId=${userDetailes.state.id}`, {
+            //     method: 'PUT',
+            //     // body: JSON.stringify({userTodos}),
+            //     body: JSON.stringify(userTodos),
+            //     // body: userTodos,
+            //     headers: {
+            //         'Content-type': 'application/json; charset=UTF-8',
+            //     },
+            // }).then(response => {
+            //     if (!response.ok) {
+            //         throw 'Error' + response.status + ': ' + response.statusText;
+            //     }
+            //     alert("todos successfully updated");
+            //     // }).then(data => {
 
-            }
-        }).then(data => {
-            setNextId(data.id);
-            localStorage.setItem('currentUser', JSON.stringify(data));
-            alert("user successfully added");
-            navigate(`/home/users/${data.id}`);
-        });
+            //     //     // navigate(`/home/users/${data.id}`);
+            // });
+            fetch(`http://localhost:3000/todos?userId=${userDetailes.state.id}`, {
+                // method: 'DELETE',
+            }).then(response => {
+                if (!response.ok) {
+                    throw 'Error' + response.status + ': ' + response.statusText;
+                }
+                alert("todos successfully deleted");
+            });
 
         } catch (ex) { alert(ex); }
-        
-
     }
 
     function changeCheckBox(i) {
@@ -61,8 +63,28 @@ function Todos() {
         setUserTodos((prev) => [...prev.slice(0, i), ...prev.slice(i + 1, prev.length)])
     }
 
-    function allowEditing(i){
-        setEditable(prev => [...prev.slice(0, i), !prev[i], ...prev.slice(i + 1, prev.length)])
+    function allowEditing(i) {
+        seteditabless(prev => [...prev.slice(0, i), !prev[i], ...prev.slice(i + 1, prev.length)])
+    }
+
+    function sortTodos(event) {
+        event.preventDefault()
+        let sortArr = userTodos.slice()
+        switch (event.target.value) {
+            case "id":
+                setUserTodos(sortArr.sort((a, b) => a.id - b.id))
+                break;
+            case "alphabet":
+                setUserTodos(sortArr.sort((a, b) => a.title > b.title ? 1 : -1))
+                break;
+            case "completed":
+                setUserTodos(sortArr.sort(a => a.completed ? -1 : 1))
+                break;
+            case "random":
+                setUserTodos(sortArr.sort(() => Math.random() > 0.5 ? -1 : 1))
+                break;
+        }
+        console.log(userTodos)
     }
 
 
@@ -79,11 +101,11 @@ function Todos() {
                 })
                 .then(data => {
                     setUserTodos(data);
-                    // setUserTodos(data.map(todo => { return { ...todo, editable: false } }));
+                    // setUserTodos(data.map(todo => { return { ...todo, editables: false } }));
                     let temp = []
                     for (let i = 0; i < data.length; i++)
                         temp.push(false)
-                    setEditable(temp);
+                    seteditables(temp);
                 })
             // setGetTodos(false)
         } catch (ex) { alert(ex); }
@@ -99,10 +121,20 @@ function Todos() {
         <br /><br />
         <button onClick={() => (setShowAdditionForm(prev => !prev))}>Add task</button>
         <button onClick={submitChanges}>Submit changes</button>
+        <br />
         <label htmlFor='sort' >order by</label>
-        <select name="sort">
+        <select onChange={sortTodos} name="sort">
+            <option value="id">id</option>
             <option value="alphabet">alphabet</option>
+            <option value="completed">completed</option>
+            <option value="random">random</option>
+        </select>
 
+        <select onChange={searchTodo} name="sort">
+            <option value="id">id</option>
+            <option value="title">title</option>
+            <option value="completed">completed</option>
+            <option value="all">all</option>
         </select>
 
         {showAdditionForm && <form onSubmit={addingTask}>
@@ -113,19 +145,29 @@ function Todos() {
 
         <br />
 
-        {userTodos.length == 0 ? <h2>There are no tasks</h2> :
+        {todo == null ? (userTodos.length == 0 ? <h2>There are no tasks</h2> :
             userTodos.map((todo, i) => {
                 return (
                     <div key={i}>
                         <span>{i + 1}. </span>
-                        {editable[i] && <input type="text" defaultValue={todo.title} style={{ width: 300 }} />}
-                        {!editable[i] && <span>{todo.title} </span>}
-                        <input type="checkbox" disabled={!editable[i]} defaultChecked={todo.completed} onChange={() => changeCheckBox(i)} />
+                        {editables[i] && <input type="text" defaultValue={todo.title} style={{ width: 300 }} />}
+                        {!editables[i] && <span>{todo.title} </span>}
+                        <input type="checkbox" disabled={!editables[i]} checked={todo.completed} onChange={() => changeCheckBox(i)} />
+                        {/* <input type="checkbox" disabled={!editables[i]} defaultChecked={todo.completed} onChange={() => changeCheckBox(i)} /> */}
                         <img src={edit} onClick={() => allowEditing(i)} />
                         <img onClick={() => deleteTask(i)} src={trash} />
                         <br /><br />
                     </div>)
-            })
+            }))
+            : <div>
+                {editables[i] && <input type="text" defaultValue={todo.title} style={{ width: 300 }} />}
+                {!editables[i] && <span>{todo.title} </span>}
+                <input type="checkbox" disabled={!editables[i]} checked={todo.completed} onChange={() => changeCheckBox(i)} />
+                {/* <input type="checkbox" disabled={!editables[i]} defaultChecked={todo.completed} onChange={() => changeCheckBox(i)} /> */}
+                <img src={edit} onClick={() => allowEditing(i)} />
+                <img onClick={() => deleteTask(i)} src={trash} />
+                <br /><br />
+            </div>
         }</>)
     // }
 }
