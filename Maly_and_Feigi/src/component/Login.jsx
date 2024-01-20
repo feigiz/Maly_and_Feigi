f28bb31a140efe8db0ccfec935b031b09a7445a9
Kibelevich <Hand4c28@mbjcomp.org.il>
2024-01-20 20:54:15 +0200

    מלי מוצ"ש

--- Maly_and_Feigi/src/component/Login.jsx
+++ Maly_and_Feigi/src/component/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 
 function Login() {
@@ -12,11 +13,23 @@ function Login() {
             navigate(`/home/users/${currentUser.id}`);
     }, [])
 
-    function onSubmit(event) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = (event) => {
         event.preventDefault();
         const { name, password } = event.target;
         checkUser(name.value, password.value);
-    }
+    };
+
+    // function onSubmit(event) {
+    //     event.preventDefault();
+    //     const { name, password } = event.target;
+    //     checkUser(name.value, password.value);
+    // }
 
     async function checkUser(name, password) {
         try {
@@ -24,26 +37,35 @@ function Login() {
             const user = await response.json();
             if (!response.ok)
                 throw 'Error' + response.status + ': ' + response.statusText;
-            if (!user[0]) 
+            if (!user[0])
                 alert("incorrect data, you have to signup");
             else {
                 console.log(user[0])
                 localStorage.setItem('currentUser', JSON.stringify(user[0]));
                 navigate(`/home/users/${user[0].id}`);
             }
-        }catch (ex) { alert(ex) }
+        } catch (ex) { alert(ex) }
     }
 
+
+    // return (<>
+    //     <form onSubmit={onSubmit}>
+    //         <label htmlFor='name' >user name</label>
+    //         <input name='name' type='text' required></input>
+    //         <label htmlFor='password' >password</label>
+    //         <input name='password' type='password' required></input>
+    //         <button type='submit'>enter</button>
+    //     </form>
+    // </>)
     return (<>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor='name' >user name</label>
-            <input name='name' type='text' required></input>
+            <input name='name' type='text' required {...register('name')}></input>
             <label htmlFor='password' >password</label>
-            <input name='password' type='password' required></input>
+            <input name='password' type='password' required {...register('password')}></input>
             <button type='submit'>enter</button>
         </form>
     </>)
-
 }
 
 export default Login;
