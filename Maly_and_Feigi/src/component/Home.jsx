c83871f332326816f7dfc8d48f4298f2474d898e
Kibelevich <Hand4c28@mbjcomp.org.il>
2024-01-22 23:42:01 +0200

    *

--- Maly_and_Feigi/src/component/Home.jsx
+++ Maly_and_Feigi/src/component/Home.jsx
@@ -1,39 +1,48 @@
 import React, { useContext, useEffect } from "react";
-import { NavLink, Outlet, useNavigate } from 'react-router-dom';
+import { NavLink, Outlet, useNavigate, useParams } from 'react-router-dom';
 import { AppContext } from "../App";
+
+
 function Home() {
-    const { userDetails, setUserDetails } = useContext(AppContext)
-    // setUserDetails(JSON.parse(localStorage.getItem('currentUser')))
-    // const userDetails = JSON.parse(localStorage.getItem('currentUser'));
-    const navigate = useNavigate();
+        const { userDetails, setUserDetails } = useContext(AppContext)
+        const navigate = useNavigate();
+        const { id } = useParams();
 
-    function logout() {
-        localStorage.clear()
-        navigate("/")
-    }
+        function logout() {
+                localStorage.clear()
+                navigate("/")
+        }
 
-    useEffect(() => {
-        if (userDetails == null)
-            setUserDetails(JSON.parse(localStorage.getItem('currentUser')))
-    }, [])
+        useEffect(() => {
+                if (userDetails == null)
+                        fetch(`http://localhost:3000/users/${id}`)
+                                .then(response => {
+                                        if (!response.ok)
+                                                throw 'Error' + response.status + ': ' + response.statusText;
+                                        return response.json();
+                                })
+                                .then(data => {
+                                        setUserDetails(data);
+                                }).catch(ex => alert(ex))
+        }, [])
 
-    return (userDetails && <>
-        <h1>welcome {userDetails.name}</h1>
-        <NavLink to="./info"
-                style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
-                Info</NavLink>
-        <NavLink to="./todos"
-                style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
-                Todos</NavLink>
-        <NavLink to="./posts"
-                style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
-                Posts</NavLink>
-        <NavLink to="./albums"
-                style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
-                Albums</NavLink>
-        <button onClick={logout}>Logout</button>
-        <Outlet />
-    </>);
+        return (userDetails && <>
+                <h1>welcome {userDetails.name}</h1>
+                <NavLink to="./info"
+                        style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
+                        Info</NavLink>
+                <NavLink to="./todos"
+                        style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
+                        Todos</NavLink>
+                <NavLink to="./posts"
+                        style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
+                        Posts</NavLink>
+                <NavLink to="./albums"
+                        style={({ isActive }) => isActive ? { border: "3px solid green", borderRadius: "10px", padding: "10px" } : {}}>
+                        Albums</NavLink>
+                <button onClick={logout}>Logout</button>
+                <Outlet />
+        </>);
 }
 
 export default Home;
