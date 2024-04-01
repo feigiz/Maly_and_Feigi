3553ead2087f6df484a932abc3accb9722176260
feigi <pz0933@gmail.com>
2024-04-01 14:40:49 +0300

    2

--- Maly_and_Feigi/src/component/Login.jsx
+++ Maly_and_Feigi/src/component/Login.jsx
@@ -17,48 +17,62 @@ function Login() {
     }, [])
 
     function loginUser(name, password) {
-        try {
-            //  const response = await fetch(`http://localhost:8080/users?username=${name}&&website=${password}`);
+        //  const response = await fetch(`http://localhost:8080/users?username=${name}&&website=${password}`);
 
-            // function addTodo(event) {
-            //     event.preventDefault();
-            //     const newTodo = { userId: userDetails.id, id: `${nextId}`, title: event.target[0].value, completed: false }
-            //     fetch('http://localhost:3000/todos', {
-            //         method: 'POST',
-            //         body: JSON.stringify(newTodo),
-            //         headers: { 'Content-type': 'application/json; charset=UTF-8' },
-            //     }).then(response => {
-            //         if (!response.ok)
-            //             throw 'Error' + response.status + ': ' + response.statusText;
-            //     }).then(() => {
-            //         setOriginalTodos(prev => [...prev, newTodo])
-            //         setTodos(prev => [...prev, { ...newTodo, originalIndex: originalTodos.length, editable: false }])
-            //         setShowAdditionForm(false)
-            //         setNextId(prevId => prevId + 1)
-            //     }).catch((ex) => alert(ex));
-            // }
+        // function addTodo(event) {
+        //     event.preventDefault();
+        //     const newTodo = { userId: userDetails.id, id: `${nextId}`, title: event.target[0].value, completed: false }
+        //     fetch('http://localhost:3000/todos', {
+        //         method: 'POST',
+        //         body: JSON.stringify(newTodo),
+        //         headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        //     }).then(response => {
+        //         if (!response.ok)
+        //             throw 'Error' + response.status + ': ' + response.statusText;
+        //     }).then(() => {
+        //         setOriginalTodos(prev => [...prev, newTodo])
+        //         setTodos(prev => [...prev, { ...newTodo, originalIndex: originalTodos.length, editable: false }])
+        //         setShowAdditionForm(false)
+        //         setNextId(prevId => prevId + 1)
+        //     }).catch((ex) => alert(ex));
+        // }
 
-            fetch(`http://localhost:8080/users/${name}`, {
-                method: 'POST',
-                body: JSON.stringify(password),
-                headers: { 'Content-type': 'application/json; charset=UTF-8' },
-            }).then(response => {
-                if (!response.ok)
-                    throw 'Error' + response.status + ': ' + response.statusText;
-            })
-            // const json = response.json();
-            const user = await json[0];
+
+        // useEffect(() => {
+        //     // fetch(`http://localhost:3000/todos?userId=${userDetails.id}`)
+        //     fetch(`http://localhost:8080/todos?userId=${userDetails.id}`)
+        //         .then(response => {
+        //             if (!response.ok)
+        //                 throw 'Error' + response.status + ': ' + response.statusText;
+        //             return response.json();//איך?
+        //         })
+        //         .then(data => {
+        //             setOriginalTodos(data);
+        //             let todosArr = []
+        //             for (let i = 0; i < data.length; i++)
+        //                 todosArr.push({ ...data[i], originalIndex: i, editable: false })
+        //             setTodos(todosArr);
+        //         }).catch(ex => alert(ex))
+        // }, [])
+        // const json = response.json();
+
+        fetch(`http://localhost:8080/users/${name}`, {
+            method: 'POST',
+            body: JSON.stringify({ password: password }),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        }).then(response => {
             if (!response.ok)
                 throw 'Error' + response.status + ': ' + response.statusText;
+            return response.json();
+        }).then(user => {
             if (!user)
                 throw 'incorrect data, you have to signup'
             else {
-                delete user.website;
                 setUserDetails(user);
                 localStorage.setItem('currentUser', JSON.stringify(user));
                 navigate(`/home/users/${user.id}`);
             }
-        } catch (ex) { alert(ex) }
+        }).catch(ex => alert(ex))
     }
 
     return (<>
